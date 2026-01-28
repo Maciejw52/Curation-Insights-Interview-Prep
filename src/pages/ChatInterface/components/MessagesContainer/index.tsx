558fb5307c7c2c1af5c070a0246c922eb4f171b9
Maciej <mwyszogrodzki52@gmail.com>
2026-01-28 22:33:48 +0300

    update read me and add scroll handler for new messages

--- src/pages/ChatInterface/components/MessagesContainer/index.tsx
+++ src/pages/ChatInterface/components/MessagesContainer/index.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useRef } from "react";
 import type { TChatMessages } from "../../index.types";
 
 interface IMessagesContainer {
   messages: TChatMessages;
 }
 
-export const MessagesContainer = ({ messages }: IMessagesContainer) => (
-  <div className="flex flex-col max-w-[60vw] overflow-y-auto min-w-[60vw] mx-auto">
-    {messages.map((message, idx) => (
-      <div key={idx} className="p-2">
-        {idx + 1}. {message.text}
-      </div>
-    ))}
-  </div>
-);
+export const MessagesContainer = ({ messages }: IMessagesContainer) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // Ensure scroll down everytime messages is updated
+  // Can later change this to only scrolldown undef certain conditions
+  useEffect(() => {
+    const currentRef = containerRef.current;
+    if (currentRef) {
+      currentRef.scrollTop = currentRef.scrollHeight;
+    }
+  }, [messages]);
+  return (
+    <div
+      ref={containerRef}
+      className="flex flex-col max-w-[60vw] overflow-y-auto min-w-[60vw] mx-auto"
+    >
+      {messages.map((message, idx) => (
+        <div key={idx} className="p-2">
+          {idx + 1}. {message.text}
+        </div>
+      ))}
+    </div>
+  );
+};
